24d5ce95e63d5103d40282cea8ff3db40132fbed
tsubakimoto_s <tkmbmbr@gmail.com>
2014-06-19 23:28:56 +0900

    TypeScriptに書き換え。

---
+++ go_dutch_ts/myscript.js
@@ -0,0 +1,44 @@
+﻿var Dutch = (function () {
+    function Dutch(amount, num, p) {
+        this.amount = amount;
+        this.num = num;
+        this.result = p;
+    }
+    Dutch.prototype.toNumber = function (s) {
+        return +s;
+    };
+
+    Dutch.prototype.calc = function () {
+        var amount = this.toNumber(this.amount.value);
+        var num = this.toNumber(this.num.value);
+        var unit = 100;
+
+        this.result.innerHTML = '';
+        this.writeShortage(amount, num, unit);
+        this.writeExtra(amount, num, unit);
+    };
+
+    Dutch.prototype.writeShortage = function (amount, num, unit) {
+        var x = Math.floor((amount / num) / unit) * unit;
+        var y = amount - (x * num);
+        this.result.innerHTML += '一人' + x + '円だと' + y + '円足りません。';
+    };
+
+    Dutch.prototype.writeExtra = function (amount, num, unit) {
+        var x = Math.ceil((amount / num) / unit) * unit;
+        var y = (x * num) - amount;
+        this.result.innerHTML += '一人' + x + '円だと' + y + '円余ります。';
+    };
+    return Dutch;
+})();
+
+window.onload = function () {
+    var amount = document.getElementById('amount');
+    var num = document.getElementById('num');
+    var result = document.getElementById('result');
+    var dutch = new Dutch(amount, num, result);
+    document.getElementById('calc').onclick = function () {
+        return dutch.calc();
+    };
+};
+//# sourceMappingURL=myscript.js.map
---
+++ go_dutch_ts/myscript.ts
@@ -0,0 +1,45 @@
+﻿class Dutch {
+    amount: HTMLInputElement;
+    num: HTMLInputElement;
+    result: HTMLElement;
+
+    constructor(amount: HTMLInputElement, num: HTMLInputElement, p: HTMLElement) {
+        this.amount = amount;
+        this.num = num;
+        this.result = p;
+    }
+
+    toNumber(s: string) {
+        return +s;
+    }
+
+    calc() {
+        var amount = this.toNumber(this.amount.value);
+        var num = this.toNumber(this.num.value);
+        var unit = 100;
+
+        this.result.innerHTML = '';
+        this.writeShortage(amount, num, unit);
+        this.writeExtra(amount, num, unit);
+    }
+
+    writeShortage(amount, num, unit) {
+        var x = Math.floor((amount / num) / unit) * unit;
+        var y = amount - (x * num);
+        this.result.innerHTML += '一人' + x + '円だと' + y + '円足りません。';
+    }
+
+    writeExtra(amount, num, unit) {
+        var x = Math.ceil((amount / num) / unit) * unit;
+        var y = (x * num) - amount;
+        this.result.innerHTML += '一人' + x + '円だと' + y + '円余ります。';
+    }
+}
+
+window.onload = () => {
+    var amount = <HTMLInputElement>document.getElementById('amount');
+    var num = <HTMLInputElement>document.getElementById('num');
+    var result = document.getElementById('result');
+    var dutch = new Dutch(amount, num, result);
+    document.getElementById('calc').onclick = () => dutch.calc();
+}
